340898778fb94abad8fb02f727d6af1ac6275eb2
Jacky Hung <jacky85725@gmail.com>
2023-04-26 14:08:34 +0700

    setting connection to db

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// only using .env file while in non-production environment
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
 // require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 const app = express()
+const mongoose  = require('mongoose')
 const port = 3000
+
+// setting connection to database
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongoose error!')
+})
+
+db.once('connection', () => {
+    console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -10,7 +28,7 @@ app.set('view engine', 'handlebars')
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', )
+  res.render('index')
 })
 
 // start and listen on the Express server
