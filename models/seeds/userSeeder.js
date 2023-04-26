5173c70d85f17f3a96d09fe23c852d95e08dddd9
Jacky Hung <jacky85725@gmail.com>
2023-04-26 15:32:39 +0700

    feat: add user seeder

---
+++ models/seeds/userSeeder.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const User = require('../user')
+const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+   ]
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  User.create(users)
+    .then(console.log('done'))
+    .catch(err => console.error(err))
+})
+
